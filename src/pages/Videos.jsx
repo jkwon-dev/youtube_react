57a5d5ce480dbabaf22eb228d80cf280b4cebfac
jkwon-dev <kwonejk@gmail.com>
2024-05-12 11:58:10 +0900

    fake youtube api

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
-import { search } from "../api/youtube";
+import FakeYoutube from "../api/fakeYoutube";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const youtube = new FakeYoutube();
   const {
     isLoading,
     error,
     data: videos,
   } = useQuery({
     queryKey: ["videos", keyword],
-    queryFn: () => search(keyword),
+    queryFn: () => youtube.search(keyword),
   });
   return (
     <>
